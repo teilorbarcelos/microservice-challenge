d47eda39c8f573664e282e9a42170dc694f4749f
Teilor Souza Barcelos <teilor.barcelos@uds.com.br>
2022-11-15 19:32:07 -0300

    base modules created, challenges and plays modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ChallengesModule } from './challenges/challenges.module';
+import { PlaysModule } from './plays/plays.module';
+
+const configService = new ConfigService();
+const DB_URL = configService.get<string>('DB_URL');
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(DB_URL, {
+      // useNewUrlParses: true,
+      // useCreateIndex: true,
+      useUnifiedTopology: true,
+      // useFindAndModify: false,
+    }),
+    ChallengesModule,
+    PlaysModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
+import * as momentTimezone from 'moment-timezone';
+
+const logger = new Logger('Main');
+const configService = new ConfigService();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const RABBITMQ_USER = configService.get<string>('RABBITMQ_USER');
+  const RABBITMQ_PASSWORD = configService.get<string>('RABBITMQ_PASSWORD');
+  const RMQ_SERVER_URL = configService.get<string>('RMQ_SERVER_URL');
+
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://${RABBITMQ_USER}:${RABBITMQ_PASSWORD}@${RMQ_SERVER_URL}`],
+      noAck: false,
+      queue: 'challenges',
+    },
+  });
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
+  await app.listen();
 }
 bootstrap();
